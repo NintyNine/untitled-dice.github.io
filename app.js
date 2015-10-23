04fc9e29e3605da1eb66c6ffd71dc052235fb8fe
NintyNine <rigged@protonmail.ch>
2015-10-23 06:56:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: '99 Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6Lfmaw8TAAAAANOHP4RgdpGbAFLrEIjjVHQahY_0',  // <----- EDIT ME!
+  recaptcha_sitekey: '6Lfmaw8TAAAAAKFrpUwt1uKoXVxfJTKbklGf4Cq6',  // <----- EDIT ME!
   redirect_uri: 'https://NintyNine.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
